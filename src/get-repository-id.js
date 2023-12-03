426bb44965053f066119d93e6ccc6486db142668
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-12-03 19:51:52 +0100

    Fix octokit call again

--- src/get-repository-id.js
+++ src/get-repository-id.js
@@ -2,7 +2,7 @@
  * Function to get the ID of the specified repo
  */
 const core = require('@actions/core')
-const octokit = require('octokit')
+const { Octokit, App } = require('octokit')
 const { inputHelper } = require('./input-helper')
 
 async function getRepositoryId() {
@@ -16,7 +16,7 @@ async function getRepositoryId() {
     owner: repo.owner,
     name: repo.name
   }
-  const result = await octokit.graphql(query, variables)
+  const result = await Octokit.graphql(query, variables)
   const repoId = result.repository.id
   return repoId
 }
